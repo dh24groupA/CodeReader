b7d7ff7967e9ef8c1b47f4e933b2d2ec52e24b6f
yuchi8313 <yuu08142003@icloud.com>
2024-11-05 20:53:19 +0900

    Initial Commit

---
+++ CodeReader/CodeReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodeReaderApp.swift
+//  CodeReader
+//
+//  Created by 植松優羽 on 2024/11/05.
+//
+
+import SwiftUI
+
+@main
+struct CodeReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CodeReaderTests/CodeReaderTests.swift
@@ -0,0 +1,17 @@
+//
+//  CodeReaderTests.swift
+//  CodeReaderTests
+//
+//  Created by 植松優羽 on 2024/11/05.
+//
+
+import Testing
+@testable import CodeReader
+
+struct CodeReaderTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
